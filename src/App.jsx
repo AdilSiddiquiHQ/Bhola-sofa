d42675c3bc8943c2d9fdc7ce266d45ae494f6783
Ninja.69 <130167556+ninjaxhustlerr@users.noreply.github.com>
2026-08-08 00:06:12 +0530

    fix: add ScrollToTop component to fix react-router scroll position bug on page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -16,11 +16,20 @@ import { WishlistProvider } from './context/WishlistContext';
 import { CartProvider } from './context/CartContext';
 import './index.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <CartProvider>
       <WishlistProvider>
         <Router>
+          <ScrollToTop />
           <div className="app-container">
           <Navbar />
           <main>
